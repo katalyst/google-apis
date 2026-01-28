c0ef177b9ec498ddf00985f0ba3cf61bf61c9fe8
Stephen Nelson <stephen@katalyst.com.au>
2026-01-28 15:01:20 +1030

    Accept details when constructing google api errors

--- app/services/katalyst/google_apis/error.rb
+++ app/services/katalyst/google_apis/error.rb
@@ -5,15 +5,20 @@ module Katalyst
     class Error < StandardError
       attr_reader :code, :status
 
-      def initialize(code:, status:, message:)
+      def initialize(code:, status:, message:, details: nil)
         super(message)
 
         @code = code
         @status = status
+        @details = details
       end
 
       def inspect
-        "#<#{self.class.name} code=#{code.inspect} status=#{status.inspect} message=#{message.inspect}>"
+        %W[#<#{self.class.name}
+           code=#{code.inspect}
+           status=#{status.inspect}
+           message=#{message.inspect}
+           details=#{details.inspect}>].join(" ")
       end
     end
   end
--- katalyst-google-apis.gemspec
+++ katalyst-google-apis.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |spec|
   spec.name    = "katalyst-google-apis"
-  spec.version = "1.2.1"
+  spec.version = "1.2.2"
   spec.authors = ["Katalyst Interactive"]
   spec.email   = ["developers@katalyst.com.au"]
 
