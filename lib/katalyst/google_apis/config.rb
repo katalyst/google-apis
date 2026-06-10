eb87093b1959184d5ef03aa6c22cc2e70c34d3d8
Stephen Nelson <stephen@katalyst.com.au>
2026-06-10 16:39:18 +0930

    Fix dependency on deprecated ActiveSupport::Configurable

--- lib/katalyst/google_apis/config.rb
+++ lib/katalyst/google_apis/config.rb
@@ -3,20 +3,19 @@
 module Katalyst
   module GoogleApis
     class Config
-      include ActiveSupport::Configurable
+      attr_accessor :project_id, :project_number, :service_account_email, :identity_pool, :identity_provider, :recaptcha
 
-      config_accessor(:project_id) { ENV.fetch("GOOGLE_PROJECT_ID", nil) }
-      config_accessor(:project_number) { ENV.fetch("GOOGLE_PROJECT_NUMBER", nil) }
-      config_accessor(:service_account_email) { ENV.fetch("GOOGLE_SERVICE_ACCOUNT_EMAIL", nil) }
-      config_accessor(:identity_pool) { ENV.fetch("GOOGLE_OIDC_IDENTITY_POOL", nil) }
-      config_accessor(:identity_provider) { ENV.fetch("GOOGLE_OIDC_IDENTITY_PROVIDER", nil) }
-
-      config_accessor(:recaptcha) do
-        defaults           = ActiveSupport::OrderedOptions.new
-        defaults.site_key  = ENV.fetch("GOOGLE_RECAPTCHA_SITE_KEY", nil)
-        defaults.score     = ENV.fetch("GOOGLE_RECAPTCHA_SCORE", 0.5).to_f
-        defaults.test_mode = !ENV.fetch("VERIFY_RECAPTCHA", !Rails.env.local?) # rubocop:disable Rails/UnknownEnv
-        defaults
+      def initialize
+        @project_id            = ENV.fetch("GOOGLE_PROJECT_ID", nil)
+        @project_number        = ENV.fetch("GOOGLE_PROJECT_NUMBER", nil)
+        @service_account_email = ENV.fetch("GOOGLE_SERVICE_ACCOUNT_EMAIL", nil)
+        @identity_pool         = ENV.fetch("GOOGLE_OIDC_IDENTITY_POOL", nil)
+        @identity_provider     = ENV.fetch("GOOGLE_OIDC_IDENTITY_PROVIDER", nil)
+        @recaptcha             = ActiveSupport::OrderedOptions.new.tap do |defaults|
+          defaults.site_key  = ENV.fetch("GOOGLE_RECAPTCHA_SITE_KEY", nil)
+          defaults.score     = ENV.fetch("GOOGLE_RECAPTCHA_SCORE", 0.5).to_f
+          defaults.test_mode = !ENV.fetch("VERIFY_RECAPTCHA", !Rails.env.local?)
+        end
       end
     end
   end
